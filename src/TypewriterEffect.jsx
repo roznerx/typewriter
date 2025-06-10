d0074e2bb4617744bebe0eb4868095358c172ca7
unknown <ignaciorosner@gmail.com>
2025-06-09 21:40:52 -0300

    Initial commit

---
+++ src/TypewriterEffect.jsx
@@ -0,0 +1,29 @@
+const TypewriterEffect = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    // TODO Display the text with typewriter effect
+    console.log(`The sentence to display is ${data.get("sentence")}`);
+  };
+  return (
+    <div>
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          display: "flex",
+          gap: "10px",
+        }}
+      >
+        <input
+          type="text"
+          name="sentence"
+          placeholder="Type a sentence"
+          style={{ width: "300px" }}
+        />
+        <button type="submit">Display with typewriter effect</button>
+      </form>
+    </div>
+  );
+};
+
+export default TypewriterEffect;
